8c091e8bd55c16cd892c33648459b803f33956a5
Michael Cheng <mikehcheng@berkeley.edu>
2015-04-07 19:15:50 -0700

    Fix basic showcase

--- SkatePhone/src/main.js
+++ SkatePhone/src/main.js
@@ -4,7 +4,7 @@ var THEME = require("themes/flat/theme");
 var BUTTONS = require("controls/buttons");
 
 deviceURL = "";
-receivedTrick = false;
+var receivedTrick = undefined;
 
 var labelStyle = new Style( { font: "20px", color:"black" } );
 var whiteSkin = new Skin( { fill:"white" } );
@@ -12,13 +12,12 @@ var titleStyle = new Style( { font: "bold 40px", color:"black" } );
 
 Handler.bind("/startPolling", {
     onInvoke: function(handler, message){
-		handler.invoke(new Message(deviceURL + "getCount"), Message.JSON);
+    	trace("Attempting Poll\n");
+		handler.invoke(new Message(deviceURL + "getTrick"), Message.JSON);
 	},
 	onComplete: function(content, message, json){
-		trickOne.string = json.trick;
-		//trickTwo.string = json.jsonTrickTwo;
-		//trickThree.string = json.jsonTrickThree;
-		thegetTrickButton.visible = true;
+		if (json.trick != undefined)
+			trickOne.string = json.trick;
      	application.invoke( new Message("/delay"));
     }
 });
@@ -41,33 +40,12 @@ Handler.bind("/delay", {
 Handler.bind("/discover", Behavior({
 	onInvoke: function(handler, message){
 		deviceURL = JSON.parse(message.requestText).url;
-		handler.invoke(new Message(deviceURL + "getCount"), Message.JSON);
+		trace("Found the device\n");
+		handler.invoke(new Message("/startPolling"));
 	},
-	onComplete: function(content, message, json){
-		trickOne.string = json.trick;
-		thegetTrickButton.visible = true;
-     	application.invoke( new Message("/startPolling"));
-	}	
 }));
 
-var trickOne = new Label({left:0, right:0, height:30, string:"0", style: labelStyle});
-//var trickTwo = new Label({left:0, right:0, height:30, string:"0", style: labelStyle});
-//var trickThree = new Label({left:0, right:0, height:30, string:"0", style: labelStyle});
-var getTrickButton = BUTTONS.Button.template(function($){ return{
-	left: 50, right: 50, height:50, top:50,
-	contents: [
-		new Label({left:50, right:50, height:50, string:"Detect Trick", style: labelStyle})
-	],
-	behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
-		onTap: { value: function(content){
-			content.invoke(new Message(deviceURL + "reset"), Message.JSON);
-		}},
-		onComplete: { value: function(content, message, json){
-			receivedTrick = json.receivedTrick;
-		}}
-	})
-}});
-thegetTrickButton = new getTrickButton()
+var trickOne = new Label({left:0, right:0, height:30, string:"", style: labelStyle});
 
 var mainColumn = new Column({
 	left: 0, right: 0, top: 0, bottom: 0, active: true, skin: whiteSkin,
@@ -75,22 +53,17 @@ var mainColumn = new Column({
 		new Label({left:0, right:0, height:40, string:"Skatey", style: titleStyle}),
 		new Picture({aspect: "fill", height:70, top: 15, width: 70, url: "icon_skateboard.png"}),
 		new Label({left:0, right:0, height:10, top: 25, string:"Learn, Compete, Skate.", style: labelStyle}),
-		new Label({left:0, right:0, height:10, top: 45, string:"Trick One:", style: labelStyle}),
+		new Label({left:0, right:0, height:10, top: 45, string:"Previous Trick:", style: labelStyle}),
 		trickOne,
-		//new Label({left:0, right:0, height:10, top: 10, string:"Trick Two:", style: labelStyle}),
-		//trickTwo,
-		//new Label({left:0, right:0, height:10, top: 10, string:"Trick Three:", style: labelStyle}),
-		//trickThree,
-		thegetTrickButton
 	]
 });
 
 var ApplicationBehavior = Behavior.template({
 	onDisplayed: function(application) {
-		application.discover("SkateDevice.app");
+		application.discover("skatedevice");
 	},
 	onQuit: function(application) {
-		application.forget("SkateDevice.app");
+		application.forget("skatedevice");
 	},
 })
 
