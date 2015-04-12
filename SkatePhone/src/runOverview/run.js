8e365105b67fbbc0157a7dbd1f0cc3031988f630
Michael Cheng <mikehcheng@berkeley.edu>
2015-04-12 04:09:46 -0700

    Working active run tracking

--- SkatePhone/src/runOverview/run.js
+++ SkatePhone/src/runOverview/run.js
@@ -1,6 +1,15 @@
 var activeRunCon;
 var inactiveRunCon;
+
 var trickTable;
+var scoreField;
+
+var maxTimerValue = 15;
+
+var extraStyle = new Style( { font: "14px", color:"black", horizontal:"center" });
+var scoreStyle = new Style( { font:	"bold 24px", color: "black", horizontal:"center"});
+var timerStyle = new Style( { font:	"bold 24px", color: "blue", horizontal:"center"});
+var finishedStyle = new Style( { font:	"bold 24px", color: "red", horizontal:"center"});
 
 // HANDLERS
 Handler.bind("/readTrick", {
@@ -10,7 +19,7 @@ Handler.bind("/readTrick", {
 	onComplete: function(handler, message, json){
 		if (json.trick != undefined) {
 			trickTable.last.add(new trickRow({trick: json.trick}));
-			trace("Got " + json.trick);
+			scoreField.score.string = (parseInt(scoreField.score.string) + trickDictionary[json.trick]).toString();
 		}
 		handler.invoke(new Message("/delay"));
 	}
@@ -26,22 +35,39 @@ Handler.bind("/delay", {
 });
 
 // RUN SCREEN ELEMENTS
+var noLabelTable = Column.template(function($) { return { left: $.left, right: $.right, top: $.top,
+	skin: new Skin({stroke: "black", borders: {top: 2, bottom: 2, right: 2, left: 2}}),
+	contents: [
+		Column($, {left: 0, right: 0, top: 0, skin: (("tableSkin" in $) ? $.tableSkin: tableSkin), contents: []})
+	]
+}});
+
 var trickRow = Line.template(function($) { return {
 	top:0, bottom:0, left:0, right:0, height: 30, skin: boxSkin, contents: [
-		new Label({top:15, left: 30, width:150, skin:whiteSkin, style: smallLabelStyle, string: $.trick}),
-		new Label({top:15, right: 30, width: 50, skin:whiteSkin, style: infoStyle, string: trickDictionary[$.trick]})
+		Label($, {top:7, left: 5, width:280, skin:whiteSkin, style: smallLabelStyle, string: $.trick}),
+		Label($, {top:7, right: 5, width:10, skin:whiteSkin, style: infoStyle, string: trickDictionary[$.trick]})
 	]
 }});
 
 var videoContainer = Container.template(function($) { return {
-	left:0, right:0, bottom: 0, height:200, skin: blackSkin, contents: [
+	left:0, right:0, bottom: 0, height:160, skin: graySkin, contents: [
 		
 	]
 }});
 
 var scoreColumn = Column.template(function($) { return {
-	left:$.left, right:$.right, name:"scoreColumn", top:0, bottom:0, height:40, skin: boxSkin, contents: [
-		new Label({top:3, left:0, right:0}) 
+	left:$.left, right:$.right, name:"scoreColumn", top:$.top, bottom:$.bottom,
+	skin: new Skin({fill: "white", stroke: "black", borders: { bottom: 2, left: 1}}), contents: [
+		Label($, {top:3, left: 0, right:0, name: "score", style: scoreStyle, string: "0"}),
+		Label($, {bottom:3, left:0, right:0, style: extraStyle, string: "points"})
+	]
+}});
+
+var timerColumn = Column.template(function($) { return {
+	left:$.left, right:$.right, top:$.top, bottom:$.bottom, 
+	skin: new Skin({fill: "white", stroke: "black", borders: { bottom: 2, right: 1}}), contents: [
+		Label($,{top:3, left: 0, right:0, style: timerStyle, behavior: Object.create(timerBehavior.prototype)}),
+		Label($,{bottom:3, left:0, right:0, style: extraStyle, string: "remaining"})
 	]
 }});
 
@@ -50,7 +76,7 @@ var scoreColumn = Column.template(function($) { return {
 var timerValue = 0;
 var timerBehavior = Behavior.template({
 	onCreate: function(container, data) {
-		timerValue = 15;
+		timerValue = maxTimerValue;
 		container.interval = 1000;
 	    container.string = timeString(timerValue);
 	    container.invoke(new Message("/readTrick"));
@@ -58,9 +84,10 @@ var timerBehavior = Behavior.template({
 	},
 	onTimeChanged: function(container) {
 		timerValue--;
+		if (timerValue <= 5)
+			container.style = finishedStyle;
 		if (timerValue == 0) {
 			container.stop();
-			container.style = new Style({color: "red", font: "14px"});
 			//activeRunCon.backButton.visible = true; //when implemented
 		}
 		container.string = timeString(timerValue);
@@ -75,23 +102,25 @@ function timeString(time) {
     return "" + minutes + ":" + seconds;
 }
 
-var timerColumn = Column.template(function($) { return {
-	left:$.left, right:$.right, top:0, bottom:0, height:40, skin: boxSkin, contents: [
-		new Label({top:3, left: 0, right:0, style: smallLabelStyle, string: "Time Remaining"}),
-		new Label({top:20, left:0, right:0, style: smallLabelStyle, behavior: Object.create(timerBehavior.prototype)})
-	]
-}});
-
 // BUILDING RUNS
 
 function createActiveRun(game) {
 	var runNumber = game.myRuns.length + 1;
-	trickTable = new Table({string: "Tricks Completed", left: 10, right:10, top: 10});
+	trickTable = new noLabelTable({left: 10, right:10, top: 10, bottom: 10});
+	scoreField = new scoreColumn({left:0, right:0, top:0, bottom:0})
 	
-	activeRunCon = new Column({
-		top:0, bottom:0, left:0, right:0, skin: whiteSkin, contents: [
-			new Container({top:0, height:40, contents: [ timerColumn({left:0, right:0}) ] }),
-			new scrollContainer({top: 50, bottom: 0, contents: [trickTable],})
+	activeRunCon = new Container({
+		top:0, bottom:0, left:0, right:0, contents: [
+			new scrollContainer({left:30, right:30, top: 130, bottom: 55, contents: [trickTable]}),
+			new Column({left:0, right:0, top:105, height:25, skin: whiteSkin, contents: [
+				new Label({left:10, top:10, style: labelStyle, string:"Tricks Completed"}),
+			]}),
+			new Line({left:0, right:0, top:55, height:50, contents:[
+				new timerColumn({left:0, right:0, top:0, bottom:0}),
+				scoreField
+			]}),
+			new videoContainer(),
+			new Container({top: 0, left: 0, right: 0, height: 55, skin:new Skin({fill: "black"})}),
 		]
 	})
 }
