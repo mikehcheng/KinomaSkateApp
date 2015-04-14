adc8e7fad6624c8623051b4c42191f2b346e8eb3
samsikson <samuel.son@berkeley.edu>
2015-04-13 20:26:25 -0700

    add description for game types

--- SkatePhone/src/gameOpponentOverview/createGameOpponent.js
+++ SkatePhone/src/gameOpponentOverview/createGameOpponent.js
@@ -2,13 +2,15 @@ var CONTROL = require('mobile/control');
 var KEYBOARD = require('mobile/keyboard');
 var BUTTONS = require("controls/buttons");
 var SLIDERS = require('controls/sliders'); 
+var smallTextStyle = new Style( { font: "bold 13px", color:"black"} );
 
 var trickContestTemplate = BUTTONS.Button.template(function($){ return{
 	index: $.index,
     top:2, bottom:2, left: 5,  right: 5, height:50, skin: whiteSkin,
     contents: [
         new Picture({aspect: "fill", left: 34, height:70, width: 70, url: "resources/skateboard.jpg"}),
-        new Label({top: 55, left: 118, string: "Trick Contest", style: miniTitleStyle})
+        new Label({top: 55, left: 118, string: "Trick Contest", style: miniTitleStyle}),
+        new Label({top: 89, left: 110, string: "Perform tricks with other skaters", style: smallTextStyle})
     ],
      behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
      	onTap: {value : function(button){
@@ -23,7 +25,8 @@ var skateGameTemplate = BUTTONS.Button.template(function($){ return{
     top:2, bottom:2, left: 5,  right: 5, height:50, skin: whiteSkin,
     contents: [
         new Picture({aspect: "fill", left: 36, height:70, width: 70, url: "resources/horse.png"}),
-        new Label({top: 55, left: 119, string: "S.K.A.T.E", style: miniTitleStyle})
+        new Label({top: 55, left: 143, string: "S.K.A.T.E", style: miniTitleStyle}),
+        new Label({top: 89, left: 102, string: "Mimic or outperform your opponent", style: smallTextStyle})
     ],
      behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
      	onTap: {value : function(button){
@@ -37,7 +40,8 @@ var skateGameTemplate = BUTTONS.Button.template(function($){ return{
     top:2, bottom:2, left: 5,  right: 5, height:50, skin: whiteSkin,
     contents: [
         new Picture({aspect: "fill", left: 39, height:70, width: 70, url: "resources/battle.jpg"}),
-        new Label({top: 55, left: 119,string: "Battle Royale", style: miniTitleStyle})
+        new Label({top: 55, left: 119,string: "Battle Royale", style: miniTitleStyle}),
+        new Label({top: 89, left: 130, string: "Be the first to land a trick", style: smallTextStyle})
     ],
      behavior: Object.create(BUTTONS.ButtonBehavior.prototype, {
      	onTap: {value : function(button){
